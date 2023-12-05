bf3f41b67a079d752f662963065a5f30029c00a6
kacsokz <caseysokach@gmail.com>
2023-12-05 15:24:26 -0700

    add batch association models

---
+++ HubSpot.NET/Api/AssociationLabelTypes.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace HubSpot.NET.Api
+{
+    public class AssociationLabelTypes
+    {
+        [DataMember(Name = "associationCategory")]
+        public AssociationCategoryTypes AssociationCategory { get; set; }
+
+        [DataMember(Name = "associationTypeId")]
+        public int AssociationTypeId { get; set; }
+    }
+
+    public enum AssociationCategoryTypes { USER_DEFINED, HUBSPOT_DEFINED }
+}
---
+++ HubSpot.NET/Api/BatchAssociationLabelsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace HubSpot.NET.Api
+{
+    public class BatchAssociationLabelsRequest
+    {
+        [DataMember(Name = "inputs")]
+        public List<BatchAssociationLabelsInputList> Inputs { get; set; }
+    }
+
+    public enum AssociationObjectType { contact, company, deal }
+}
